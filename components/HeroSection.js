bb7dd9c7236aea0ad52b1c77f27821ee69728e5d
juanzenn <juanandres140299@gmail.com>
2021-07-06 20:14:05 -0400

    Initial setup and my nice tailwind oh yea

---
+++ components/HeroSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import marked from "marked";
+import DOMPurify from "dompurify";
+
+export default function HeroSection(props) {
+  const { content } = props;
+  // Sanitize that body bois
+
+  return (
+    <section className='container mx-auto px-4 space-y-10 py-24 md:grid md:grid-cols-2 md:items-center md:gap-10 md:py-12'>
+      <article className='space-y-6'>
+        <div className='mb-10'>
+          <h2 className='text-2xl md:text-4xl text-gray-400 font-black tracking-tighter leading-none'>{content.title}</h2>
+          <h1 className='text-5xl md:text-7xl text-gray-800 font-black tracking-tighter leading-none'> {content.subtitle} </h1>
+        </div>
+        <span className='tracking-wide font-light text-xl' dangerouslySetInnerHTML={{ __html: marked(content.body) }}></span>
+        <a className='block bg-primary-800 font-bold tracking-tight text-white px-6 py-4 w-max md:text-lg hover:bg-primary-700 shadow-md' href={`${content.callToActionLink}`}>{content.callToActionLabel}</a>
+      </article>
+      <figure className='mx-auto w-full h-full px-4 md:p-12'>
+        <Image
+          src={content.image.url}
+          alt={content.image.alt}
+          width='800px'
+          height='800px'
+          layout='responsive'
+        />
+      </figure>
+    </section>
+  );
+}
---
+++ dato.js
@@ -0,0 +1,191 @@
+const fetch = require('node-fetch')
+
+const APItoken = process.env.DATO_CMS_TOKEN
+
+// This file contains all my queries for DATOCMS
+// Boiler plate for fetching 
+const fetchQuery = async (queryString) => {
+  const data = await fetch('https://graphql.datocms.com/', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${APItoken}`
+    },
+    body: JSON.stringify({
+      query: queryString
+    })
+  })
+
+  return data
+}
+
+// Fetch for Homepage Content
+const fetchHomepage = async () => {
+  const queryString = `{
+    homepage(locale: en) {
+      homepageContent {
+        ... on HeroSectionRecord {
+          id
+          __typename
+          title
+          subtitle
+          body
+          callToActionLink
+          callToActionLabel
+          image {
+            url
+            alt
+          }
+        }
+        ... on TwoColRecord {
+          id
+          __typename
+          firstColumn
+          secondColumn
+        }
+      }
+    }
+  }  
+  `
+  const homepageData = await fetchQuery(queryString)
+  const homepage = await homepageData.json()
+  return homepage.data.homepage.homepageContent
+}
+
+// Fetch the Main Navbar
+const fetchMainNavbar = async () => {
+  const queryString = `{
+    mainMenu(locale: en) {
+      content {
+        id
+        link
+        label
+      }
+    }
+  }
+  
+  `
+  const mainNavbarData = await fetchQuery(queryString)
+  const mainNavbar = await mainNavbarData.json()
+  return mainNavbar.data.mainMenu.content
+}
+
+// Fetch Project Cards
+const fetchProjectCards = async () => {
+  const queryString = `{
+    allProjectCards(locale: en) {
+      id
+      title
+      category
+      projectLink
+      image {
+        url
+        alt
+      }
+    }
+  }`
+
+  const projectCardsData = await fetchQuery(queryString)
+  const projectCards = await projectCardsData.json()
+  return projectCards.data.allProjectCards
+}
+
+// Fetch services Cards 
+const fetchServicesCards = async () => {
+  const queryString = `{
+    allServiceCards(locale: en) {
+      id
+      title
+      body
+      link
+      image {
+        alt
+        url
+      }
+    }
+  }`
+
+  const serviceCardsData = await fetchQuery(queryString)
+  const serviceCards = await serviceCardsData.json()
+  return serviceCards.data.allServiceCards
+}
+
+const fetchFooterGroups = async () => {
+  const queryString = `{
+    allFooterGroups(locale:en) {
+      id
+      title
+      content {
+        label
+        link
+      }
+    }
+  }`
+
+  const footerGroupsData = await fetchQuery(queryString)
+  const footerGroups = await footerGroupsData.json()
+  return footerGroups.data.allFooterGroups
+}
+
+const fetchContactSections = async () => {
+  const queryString = `{
+    contact(locale: en) {
+      content {
+        ... on ContactInfoRecord {
+          id
+          __typename
+          title
+          content
+        },
+        ... on ContactSectionRecord {
+          id
+          __typename
+          title
+          subtitle
+        },
+        ... on SocialRecord {
+          id
+          __typename
+          link
+          icon {
+            url
+            alt
+          }
+        },
+        ... on QuoteRecord {
+          id
+          __typename
+          quote,
+          author
+        }
+      }
+    }
+  }`
+
+  const conctactSectionsData = await fetchQuery(queryString)
+  const contactSections = await conctactSectionsData.json()
+  return contactSections.data.contact.content
+}
+
+const indexFetch = async () => {
+  const homepage = await fetchHomepage()
+  const mainNavbar = await fetchMainNavbar()
+  const projectCards = await fetchProjectCards()
+  const serviceCards = await fetchServicesCards()
+  const footerGroups = await fetchFooterGroups()
+  const contactSections = await fetchContactSections()
+  return {
+    homepage,
+    mainNavbar,
+    projectCards,
+    serviceCards,
+    footerGroups,
+    contactSections
+  }
+}
+
+
+module.exports = {
+  indexFetch
+}
